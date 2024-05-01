6f0e15effa24bbc8a3e7dc405e37350c3cdd07e8
Julia <jukl@itu.dk>
2024-05-01 21:51:56 +0200

    kan nu også slette event igen med billede

--- app/src/main/java/dk/itu/moapd/copenhagenbuzz/astb/adapters/EventAdapter.kt
+++ app/src/main/java/dk/itu/moapd/copenhagenbuzz/astb/adapters/EventAdapter.kt
@@ -1,23 +1,35 @@
 package dk.itu.moapd.copenhagenbuzz.astb.adapters
 
+import DeleteEventDialogFragment
 import android.content.Context
+import android.location.Address
+import android.location.Geocoder
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.Button
+import android.widget.CheckBox
 import android.widget.TextView
 import android.widget.ImageView
 import androidx.fragment.app.FragmentManager
+import androidx.recyclerview.widget.RecyclerView
 import com.firebase.ui.database.FirebaseListAdapter
 import com.firebase.ui.database.FirebaseListOptions
 import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.database.DatabaseReference
-import dk.itu.moapd.copenhagenbuzz.astb.fragments.DeleteEventDialogFragment
+import com.google.firebase.storage.storage
+import com.squareup.picasso.Picasso
+import dk.itu.moapd.copenhagenbuzz.astb.BUCKET_URL
+import com.google.firebase.database.ktx.database
+import com.google.firebase.ktx.Firebase
+import dk.itu.moapd.copenhagenbuzz.astb.DATABASE_URL
 import dk.itu.moapd.copenhagenbuzz.astb.R
 import dk.itu.moapd.copenhagenbuzz.astb.fragments.UpdateEventDialogFragment
+import dk.itu.moapd.copenhagenbuzz.astb.interfaces.OnFavoriteClickListener
 import dk.itu.moapd.copenhagenbuzz.astb.models.Event
+import java.util.Locale
 
-class EventAdapter(private val fragmentManager: FragmentManager, private val context: Context, private val options: FirebaseListOptions<Event>) :
+class EventAdapter(private val fragmentManager: FragmentManager, private val context: Context, private val options: FirebaseListOptions<Event>, private val  onFavoriteClickListener: OnFavoriteClickListener) :
     FirebaseListAdapter<Event>(options) {
 
     private var auth: FirebaseAuth = FirebaseAuth.getInstance()
@@ -50,13 +62,20 @@ class EventAdapter(private val fragmentManager: FragmentManager, private val con
 
     private fun populateViewHolder(viewHolder: ViewHolder, event: Event, position: Int) {
         with(viewHolder) {
-            eventIcon.setImageResource(R.drawable.baseline_map_24)
+            bindEvent(viewHolder, event)
+
+            bindFavorites(viewHolder, event, position)
+            /*
+            //eventIcon.setImageResource(R.drawable.baseline_map_24)
             eventName.text = event.eventName
-            eventLocation.text = event.eventLocation
+            eventLocation.text = event.eventLocation?.address
             eventDate.text = event.startDate.toString()
             eventType.text = event.eventType
             eventDescription.text = event.eventDescription
+            loadImageToView(event, eventIcon)
 
+
+             */
             val currentUser = auth.currentUser
             val currentUserUid = currentUser?.uid
             val eventUserId = event.userId
@@ -64,7 +83,7 @@ class EventAdapter(private val fragmentManager: FragmentManager, private val con
             if(currentUserUid == eventUserId) {
                 editButton?.visibility = View.VISIBLE
                 editButton?.setOnClickListener {
-                    UpdateEventDialogFragment(event, position, getId(position)).apply {
+                    UpdateEventDialogFragment(event, position, this@EventAdapter).apply {
                         isCancelable = false
                     }.show(fragmentManager, "UpdateEventFragment")
                 }
@@ -81,14 +100,46 @@ class EventAdapter(private val fragmentManager: FragmentManager, private val con
             }
         }
     }
+    private fun bindEvent(viewHolder: ViewHolder, event: Event) {
+        with(viewHolder){
+            eventIcon.setImageResource(R.drawable.baseline_map_24)
+            eventName.text = event.eventName
+            eventLocation.text = event.eventLocation?.address
+            eventDate.text = event.startDate.toString()
+            eventType.text = event.eventType
+            eventDescription.text = event.eventDescription
+        }
+    }
 
+    private fun bindFavorites(viewHolder: ViewHolder, event: Event, position: Int){
+
+        auth.currentUser?.uid?.let { userId ->
+            with(viewHolder) {
+                val ref = getId(position)
+                ref.key?.let {
+                    onFavoriteClickListener.isFavorite(it) { exists ->
+                        favoriteCheckbox.isChecked = exists
+                    }
+                }
+
+                favoriteCheckbox.setOnCheckedChangeListener { _, isChecked ->
+
+                    onFavoriteClickListener.onFavoriteClick(ref, event ,isChecked)
+
+                }
+            }
+
+        }
+
+    }
 
 
 
     private class ViewHolder(view: View) {
-        val eventIcon = view.findViewById<ImageView>(R.id.event_icon)
-        val eventName = view.findViewById<TextView>(R.id.event_name)
-        val eventLocation = view.findViewById<TextView>(R.id.event_location)
+        val favoriteCheckbox= view.findViewById<CheckBox>(R.id.favorite_button)
+        val eventIcon= view.findViewById<ImageView>(R.id.event_icon)
+        val eventName= view.findViewById<TextView>(R.id.event_name)
+        val eventLocation= view.findViewById<TextView>(R.id.event_location)
         val eventDate = view.findViewById<TextView>(R.id.event_date)
         val eventType = view.findViewById<TextView>(R.id.event_type)
         val eventDescription = view.findViewById<TextView>(R.id.event_description)
@@ -102,3 +153,5 @@ class EventAdapter(private val fragmentManager: FragmentManager, private val con
         return getRef(position)
     }
 }
+
+
