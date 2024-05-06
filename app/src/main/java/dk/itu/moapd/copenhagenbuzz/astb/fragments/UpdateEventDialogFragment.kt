8d3ca1e53ab3811652cf8f8f99676b0de3c494c3
Julia <jukl@itu.dk>
2024-05-07 00:40:33 +0200

    deleted unused methods

--- app/src/main/java/dk/itu/moapd/copenhagenbuzz/astb/fragments/UpdateEventDialogFragment.kt
+++ app/src/main/java/dk/itu/moapd/copenhagenbuzz/astb/fragments/UpdateEventDialogFragment.kt
@@ -25,7 +25,6 @@ import com.android.volley.toolbox.Volley
 import com.google.android.material.datepicker.MaterialDatePicker
 import com.google.android.material.dialog.MaterialAlertDialogBuilder
 import com.google.android.material.snackbar.Snackbar
-import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.database.DatabaseReference
 import com.google.firebase.database.ktx.database
 import com.google.firebase.ktx.Firebase
@@ -51,7 +50,6 @@ class UpdateEventDialogFragment(private val event: Event,
                                 private val position: Int,
                                 private val adapter: EventAdapter, private val parentView: View) : DialogFragment() {
     private var _binding: FragmentUpdateEventBinding? = null
-    private var auth: FirebaseAuth = FirebaseAuth.getInstance()
     private lateinit var database: DatabaseReference
     private lateinit var updatedEvent: Event
     private var eventLocation: EventLocation? = event.eventLocation
@@ -165,7 +163,7 @@ class UpdateEventDialogFragment(private val event: Event,
             "dk.itu.moapd.copenhagenbuzz.astb.fileprovider",
             photoFile
         )
-        cameraIntent.putExtra(MediaStore.EXTRA_OUTPUT, photoUri)
+        cameraIntent.putExtra(MediaStore.EXTRA_OUTPUT, photoUri!!)
         takePhoto.launch(cameraIntent)
     }
 
